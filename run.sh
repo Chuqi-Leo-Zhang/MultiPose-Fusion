5143004f07b5dfce9a16f8682b69d0c6b3891a8e
Chuqi Zhang <160417230+Chuqi-Leo-Zhang@users.noreply.github.com>
2024-12-10 23:00:33 -0500

    Delete run.sh

--- run.sh
+++
@@ -1,4 +0,0 @@
-CUDA_VISIBLE_DEVICES=9 nohup python train_renderer.py -i output/aircraft/0.png \
-                         -n aircraft-nerf \
-                         -b configs/nerf.yaml \
-                         -l output/renderer> running_nerf.log 2>&1 &
